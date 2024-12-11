7f4640ae033ba0ad458aba9ba03589ced73fe71a
MuhamadAhmadin <ahmadinations@gmail.com>
2024-12-11 11:55:31 +0700

    alignment: change ckeditor to super build

--- resources/views/berita/edit.blade.php
+++ resources/views/berita/edit.blade.php
@@ -18,12 +18,14 @@
                                 @include('components.flash_messages')
 
                                 <form class="row g-3 needs-validation myForm" method="POST"
-                                    action="{{ route('dashboard.berita.update', $data->id) }}" enctype="multipart/form-data">
+                                    action="{{ route('dashboard.berita.update', $data->id) }}"
+                                    enctype="multipart/form-data">
                                     @csrf
                                     @method('PUT')
                                     <div class="form-group col-md-12 mt-3">
                                         <label>Judul Berita</label>
-                                        <input type="text" class="form-control" name="judul" value="{{ $data->judul }}" required>
+                                        <input type="text" class="form-control" name="judul"
+                                            value="{{ $data->judul }}" required>
                                     </div>
                                     <div class="form-group col-md-12 mt-3">
                                         <label>Deskripsi</label>
@@ -34,7 +36,9 @@
                                         <select name="kategori_id" id="kategori_id" class="form-control">
                                             <option value="">-- Pilih Kategori --</option>
                                             @foreach ($kategoris as $item)
-                                                <option value="{{ $item->id }}" {{ $item->id == $data->kategori_id ? 'selected' : '' }}>{{ $item->nama }}</option>
+                                                <option value="{{ $item->id }}"
+                                                    {{ $item->id == $data->kategori_id ? 'selected' : '' }}>
+                                                    {{ $item->nama }}</option>
                                             @endforeach
                                         </select>
                                     </div>
@@ -43,7 +47,8 @@
                                         <select name="visible" id="visible" class="form-control" required>
                                             <option value="">-- Pilih Status --</option>
                                             <option value="1" {{ $data->visible == 1 ? 'selected' : '' }}>Ya</option>
-                                            <option value="0" {{ $data->visible == 0 ? 'selected' : '' }}>Tidak</option>
+                                            <option value="0" {{ $data->visible == 0 ? 'selected' : '' }}>Tidak
+                                            </option>
                                         </select>
                                     </div>
                                     <div class="form-group col-md-4 mt-3">
@@ -75,67 +80,192 @@
 @endsection
 
 @push('js')
-    <script src="{{ asset('skote') }}/assets/libs/%40ckeditor/ckeditor5-build-classic/build/ckeditor.js"></script>
+    <script src="https://cdn.ckeditor.com/ckeditor5/41.4.2/super-build/ckeditor.js"></script>
 
     <script>
-        var ckClassicEditor = document.querySelectorAll(".ckeditor-classic"),
-            snowEditor = (ckClassicEditor && Array.from(ckClassicEditor).forEach(function() {
-                ClassicEditor.create(document.querySelector(".ckeditor-classic"), {
-                    ckfinder: {
-                        uploadUrl: '{{ route("dashboard.helper.ckeditor_upload") }}?_token=' + document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
-                        headers: {
-                            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+        CKEDITOR.ClassicEditor.create(document.getElementById("deskripsi"), {
+            // https://ckeditor.com/docs/ckeditor5/latest/getting-started/setup/toolbar/toolbar.html#extended-toolbar-configuration-format
+            toolbar: {
+                items: [
+                    'exportPDF', 'exportWord', '|',
+                    'findAndReplace', 'selectAll', '|',
+                    'heading', '|',
+                    'bold', 'italic', 'strikethrough', 'underline', 'code', 'subscript', 'superscript',
+                    'removeFormat', '|',
+                    'bulletedList', 'numberedList', 'todoList', '|',
+                    'outdent', 'indent', '|',
+                    'undo', 'redo',
+                    '-',
+                    'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor', 'highlight', '|',
+                    'alignment', '|',
+                    'link', 'uploadImage', 'blockQuote', 'insertTable', 'mediaEmbed', 'codeBlock', 'htmlEmbed',
+                    '|',
+                    'specialCharacters', 'horizontalLine', 'pageBreak', '|',
+                    'textPartLanguage', '|',
+                    'sourceEditing'
+                ],
+                shouldNotGroupWhenFull: true
+            },
+            // Changing the language of the interface requires loading the language file using the <script> tag.
+            // language: 'es',
+            list: {
+                properties: {
+                    styles: true,
+                    startIndex: true,
+                    reversed: true
+                }
+            },
+            // https://ckeditor.com/docs/ckeditor5/latest/features/headings.html#configuration
+            heading: {
+                options: [{
+                        model: 'paragraph',
+                        title: 'Paragraph',
+                        class: 'ck-heading_paragraph'
+                    },
+                    {
+                        model: 'heading1',
+                        view: 'h1',
+                        title: 'Heading 1',
+                        class: 'ck-heading_heading1'
+                    },
+                    {
+                        model: 'heading2',
+                        view: 'h2',
+                        title: 'Heading 2',
+                        class: 'ck-heading_heading2'
+                    },
+                    {
+                        model: 'heading3',
+                        view: 'h3',
+                        title: 'Heading 3',
+                        class: 'ck-heading_heading3'
+                    },
+                    {
+                        model: 'heading4',
+                        view: 'h4',
+                        title: 'Heading 4',
+                        class: 'ck-heading_heading4'
+                    },
+                    {
+                        model: 'heading5',
+                        view: 'h5',
+                        title: 'Heading 5',
+                        class: 'ck-heading_heading5'
+                    },
+                    {
+                        model: 'heading6',
+                        view: 'h6',
+                        title: 'Heading 6',
+                        class: 'ck-heading_heading6'
+                    }
+                ]
+            },
+            // https://ckeditor.com/docs/ckeditor5/latest/features/editor-placeholder.html#using-the-editor-configuration
+            placeholder: 'Silahkan isi konten disini ya!',
+            // https://ckeditor.com/docs/ckeditor5/latest/features/font.html#configuring-the-font-family-feature
+            fontFamily: {
+                options: [
+                    'default',
+                    'Arial, Helvetica, sans-serif',
+                    'Courier New, Courier, monospace',
+                    'Georgia, serif',
+                    'Lucida Sans Unicode, Lucida Grande, sans-serif',
+                    'Tahoma, Geneva, sans-serif',
+                    'Times New Roman, Times, serif',
+                    'Trebuchet MS, Helvetica, sans-serif',
+                    'Verdana, Geneva, sans-serif'
+                ],
+                supportAllValues: true
+            },
+            // https://ckeditor.com/docs/ckeditor5/latest/features/font.html#configuring-the-font-size-feature
+            fontSize: {
+                options: [10, 12, 14, 'default', 18, 20, 22],
+                supportAllValues: true
+            },
+            // Be careful with the setting below. It instructs CKEditor to accept ALL HTML markup.
+            // https://ckeditor.com/docs/ckeditor5/latest/features/general-html-support.html#enabling-all-html-features
+            htmlSupport: {
+                allow: [{
+                    name: /.*/,
+                    attributes: true,
+                    classes: true,
+                    styles: true
+                }]
+            },
+            // Be careful with enabling previews
+            // https://ckeditor.com/docs/ckeditor5/latest/features/html-embed.html#content-previews
+            htmlEmbed: {
+                showPreviews: false
+            },
+            // https://ckeditor.com/docs/ckeditor5/latest/features/link.html#custom-link-attributes-decorators
+            link: {
+                decorators: {
+                    addTargetToExternalLinks: true,
+                    defaultProtocol: 'https://',
+                    toggleDownloadable: {
+                        mode: 'manual',
+                        label: 'Downloadable',
+                        attributes: {
+                            download: 'file'
                         }
                     }
-                }).then(function(e) {
-                    e.ui.view.editable.element.style.height = "200px"
-                }).catch(function(e) {
-                    console.error(e)
-                })
-            }), document.querySelectorAll(".snow-editor")),
-            bubbleEditor = (snowEditor && Array.from(snowEditor).forEach(function(e) {
-                var o = {};
-                1 == e.classList.contains("snow-editor") && (o.theme = "snow", o.modules = {
-                    toolbar: [
-                        [{
-                            font: []
-                        }, {
-                            size: []
-                        }],
-                        ["bold", "italic", "underline", "strike"],
-                        [{
-                            color: []
-                        }, {
-                            background: []
-                        }],
-                        [{
-                            script: "super"
-                        }, {
-                            script: "sub"
-                        }],
-                        [{
-                            header: [!1, 1, 2, 3, 4, 5, 6]
-                        }, "blockquote", "code-block"],
-                        [{
-                            list: "ordered"
-                        }, {
-                            list: "bullet"
-                        }, {
-                            indent: "-1"
-                        }, {
-                            indent: "+1"
-                        }],
-                        ["direction", {
-                            align: []
-                        }],
-                        ["link", "image", "video"],
-                        ["clean"]
-                    ]
-                }), new Quill(e, o)
-            }), document.querySelectorAll(".bubble-editor"));
-        bubbleEditor && Array.from(bubbleEditor).forEach(function(e) {
-            var o = {};
-            1 == e.classList.contains("bubble-editor") && (o.theme = "bubble"), new Quill(e, o)
+                }
+            },
+            // https://ckeditor.com/docs/ckeditor5/latest/features/mentions.html#configuration
+            mention: {
+                feeds: [{
+                    marker: '@',
+                    feed: [
+                        '@apple', '@bears', '@brownie', '@cake', '@cake', '@candy', '@canes',
+                        '@chocolate', '@cookie', '@cotton', '@cream',
+                        '@cupcake', '@danish', '@donut', '@dragée', '@fruitcake', '@gingerbread',
+                        '@gummi', '@ice', '@jelly-o',
+                        '@liquorice', '@macaroon', '@marzipan', '@oat', '@pie', '@plum', '@pudding',
+                        '@sesame', '@snaps', '@soufflé',
+                        '@sugar', '@sweet', '@topping', '@wafer'
+                    ],
+                    minimumCharacters: 1
+                }]
+            },
+            // The "superbuild" contains more premium features that require additional configuration, disable them below.
+            // Do not turn them on unless you read the documentation and know how to configure them and setup the editor.
+            removePlugins: [
+                // These two are commercial, but you can try them out without registering to a trial.
+                // 'ExportPdf',
+                // 'ExportWord',
+                'AIAssistant',
+                'CKBox',
+                'CKFinder',
+                'EasyImage',
+                // This sample uses the Base64UploadAdapter to handle image uploads as it requires no configuration.
+                // https://ckeditor.com/docs/ckeditor5/latest/features/images/image-upload/base64-upload-adapter.html
+                // Storing images as Base64 is usually a very bad idea.
+                // Replace it on production website with other solutions:
+                // https://ckeditor.com/docs/ckeditor5/latest/features/images/image-upload/image-upload.html
+                // 'Base64UploadAdapter',
+                'MultiLevelList',
+                'RealTimeCollaborativeComments',
+                'RealTimeCollaborativeTrackChanges',
+                'RealTimeCollaborativeRevisionHistory',
+                'PresenceList',
+                'Comments',
+                'TrackChanges',
+                'TrackChangesData',
+                'RevisionHistory',
+                'Pagination',
+                'WProofreader',
+                // Careful, with the Mathtype plugin CKEditor will not load when loading this sample
+                // from a local file system (file://) - load this site via HTTP server if you enable MathType.
+                'MathType',
+                // The following features require additional license.
+                'SlashCommand',
+                'Template',
+                'DocumentOutline',
+                'FormatPainter',
+                'TableOfContents',
+                'PasteFromOfficeEnhanced',
+                'CaseChange'
+            ]
         });
     </script>
 @endpush
